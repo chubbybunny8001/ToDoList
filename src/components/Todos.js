c392fb6315b70790ee894fc8c594a20dfaf44ac7
Josiah Sanchez <josiahbear223@gmail.com>
2020-08-27 10:02:56 -0600

    First Commit

---
+++ src/components/Todos.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Todoitem from './Todoitem';
+
+class Todos extends Component {
+    render () {
+    console.log(this.props.todos)
+    return this.props.todos.map((todo) => (
+        <Todoitem todo={todo} />
+    ));
+}}
+
+export default Todos;
